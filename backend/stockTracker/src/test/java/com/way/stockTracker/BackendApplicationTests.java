c79159448415a600f7dd5208faadebe837e1f520
Way05 <130930847+Way05@users.noreply.github.com>
2025-08-04 20:18:58 -0400

    switch to maven

--- backend/stockTracker/src/test/java/com/way/stockTracker/BackendApplicationTests.java
+++ backend/stockTracker/src/test/java/com/way/stockTracker/BackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StockTrackerApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
