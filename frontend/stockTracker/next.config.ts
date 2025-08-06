793a448b89a059ebc3c6006a75cfd6b1f980f3b7
Way05 <wtan235@gmail.com>
2025-08-05 20:16:56 -0400

    removed static site generation

--- frontend/stockTracker/next.config.ts
+++ frontend/stockTracker/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  distDir: "../../backend/stockTracker/src/main/resources/static",
 };
 
 export default nextConfig;
