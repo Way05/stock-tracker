4a34192bcbb2635aaf852b51880792db1b9688f0
Way05 <wtan235@gmail.com>
2025-08-05 21:42:26 -0400

    navbar & layout

--- frontend/stockTracker/src/app/layout.tsx
+++ frontend/stockTracker/src/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Button } from "@heroui/button";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
+  variable: "--font-serif-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-serif-mono",
   subsets: ["latin"],
 });
 
@@ -25,9 +27,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} h-screen antialiased`}
       >
-        {children}
+        <Providers>
+          <header className="relative flex justify-around p-5">
+            <div className="flex items-center">
+              <h1 className="text-3xl font-bold">Stock Tracker</h1>
+            </div>
+            <nav className="flex gap-2">
+              <Button
+                radius="full"
+                className="bg-green-950 text-green-100"
+                data-hover={false}
+              >
+                Sign Up
+              </Button>
+              <Button
+                radius="full"
+                className="bg-green-950 text-green-100"
+                data-hover={false}
+              >
+                Login
+              </Button>
+            </nav>
+          </header>
+          {children}
+        </Providers>
       </body>
     </html>
   );
