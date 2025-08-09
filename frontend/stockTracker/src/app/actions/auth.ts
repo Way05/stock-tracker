4138a143d97a984cab03a204571a2e5120146f38
Way05 <wtan235@gmail.com>
2025-08-08 22:56:36 -0400

    form data send setup

--- frontend/stockTracker/src/app/actions/auth.ts
+++ frontend/stockTracker/src/app/actions/auth.ts
@@ -1,1 +1,12 @@
-export async function signup(formData: FormData) {}
+export async function signup(formData: FormData) {
+  formData.forEach((data) => {
+    if (!data) return;
+  });
+  const data: FormData = new FormData();
+  data.append("name", formData.get("user") as string);
+  data.append("password", formData.get("password") as string);
+  const response: Response = await fetch("SIGNUP_API", {
+    method: "POST",
+    body: data,
+  });
+}
