ef8be71b9721f178bd329a94eae7182678d2d46a
Way05 <wtan235@gmail.com>
2025-08-09 12:04:19 -0400

    redid constructor

--- backend/stockTracker/src/main/java/com/way/stockTracker/User.java
+++ backend/stockTracker/src/main/java/com/way/stockTracker/User.java
@@ -3,17 +3,20 @@ package com.way.stockTracker;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "users")
 public class User {
     @Id
-    private ObjectId id;
+    private String id;
     private String username;
     private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
