22f35dcd476bae10ebb9946f1ceb1f441500993f
Way05 <wtan235@gmail.com>
2025-08-09 12:03:33 -0400

    basic signup implemented

--- backend/stockTracker/src/main/java/com/way/stockTracker/AuthController.java
+++ backend/stockTracker/src/main/java/com/way/stockTracker/AuthController.java
@@ -1,6 +1,8 @@
 package com.way.stockTracker;
 
 import dto.LoginRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/signup")
     public String signup(@RequestBody LoginRequestDTO signupRequest) {
-        return null;
+        User checkExists = userService.findUserByUsername(signupRequest.getUsername());
+        if(checkExists != null) {
+            return "User already exists";
+        } else {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+            userService.createUser(new User(signupRequest.getUsername(), encoder.encode(signupRequest.getPassword())));
+            return "New user successfully created";
+        }
     }
 
     @PostMapping("/login")
