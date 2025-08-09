6321279a70e518715268eaf7b8578ffcadfedc67
Way05 <wtan235@gmail.com>
2025-08-08 23:37:09 -0400

    setup login api

---
+++ backend/stockTracker/src/main/java/com/way/stockTracker/AuthController.java
@@ -0,0 +1,22 @@
+package com.way.stockTracker;
+
+import dto.LoginRequestDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody LoginRequestDTO signupRequest) {
+        return null;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDTO loginRequest) {
+        return null;
+    }
+}
