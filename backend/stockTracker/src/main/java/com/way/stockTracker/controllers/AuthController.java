da65d8e211a2d51938b724ff775caaf92d34c56d
Way05 <wtan235@gmail.com>
2025-08-10 14:11:55 -0400

    dep injection with passwordService

--- backend/stockTracker/src/main/java/com/way/stockTracker/controllers/AuthController.java
+++ backend/stockTracker/src/main/java/com/way/stockTracker/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.way.stockTracker.controllers;
 
 import com.way.stockTracker.models.User;
+import com.way.stockTracker.services.PasswordService;
 import com.way.stockTracker.services.UserService;
 import com.way.stockTracker.dto.LoginRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +15,33 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordService passwordService;
+
     @PostMapping("/signup")
     public String signup(@ModelAttribute LoginRequestDTO signupRequest) {
-        User checkExists = userService.findUserByUsername(signupRequest.getUsername());
-        if(checkExists != null) {
+        User userExists = userService.findUserByUsername(signupRequest.getUsername());
+        if(userExists != null) {
             return "User already exists";
         } else {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
-            userService.createUser(new User(signupRequest.getUsername(), encoder.encode(signupRequest.getPassword())));
+            userService.createUser(new User(signupRequest.getUsername(), passwordService.hashPassword(signupRequest.getPassword())));
             return "New user successfully created";
         }
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDTO loginRequest) {
-        return null;
+    public String login(@ModelAttribute LoginRequestDTO loginRequest) {
+        User userExists = userService.findUserByUsername(loginRequest.getUsername());
+        if(userExists == null) {
+            return "User does not exist";
+        } else {
+            String dbPasswordHash = userExists.getPassword();
+            boolean compare = passwordService.verifyPassword(loginRequest.getPassword(), dbPasswordHash);
+            if(compare) {
+                return "Password correct";
+            } else {
+                return "Incorrect password";
+            }
+        }
     }
 }
