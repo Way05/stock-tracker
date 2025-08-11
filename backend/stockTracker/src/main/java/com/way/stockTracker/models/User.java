c1146d874f493927ad4cff13d0718369d82737fa
Way05 <wtan235@gmail.com>
2025-08-11 15:06:05 -0400

    remove optional

--- backend/stockTracker/src/main/java/com/way/stockTracker/models/User.java
+++ backend/stockTracker/src/main/java/com/way/stockTracker/models/User.java
@@ -4,16 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
-@Document(collection = "Users")
-public class User implements UserDetails {
+@Document(collection="Users")
+public class User {
     @Id
     private String id;
     private String username;
@@ -22,10 +17,5 @@ public class User implements UserDetails {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
     }
 }
--- backend/stockTracker/src/main/java/com/way/stockTracker/repositories/UserRepository.java
+++ backend/stockTracker/src/main/java/com/way/stockTracker/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import com.way.stockTracker.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.Optional;
-
 public interface UserRepository extends MongoRepository<User, String> {
     @Query("{username:'?0'}")
     User findByUsername(String username);
