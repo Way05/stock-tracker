04dcec745848c6b0e91f22f3a2232abfbafb0f79
Way05 <wtan235@gmail.com>
2025-08-11 15:03:31 -0400

    checks 200, print response

--- frontend/stockTracker/src/app/actions/auth.ts
+++ frontend/stockTracker/src/app/actions/auth.ts
@@ -2,24 +2,52 @@ export async function signup(formData: FormData) {
   formData.forEach((data) => {
     if (!data) return;
   });
+
   const data: FormData = new FormData();
   data.append("name", formData.get("user") as string);
   data.append("password", formData.get("password") as string);
-  const response: Response = await fetch("SIGNUP_API", {
-    method: "POST",
-    body: data,
-  });
+
+  try {
+    const response: Response = await fetch(
+      "http://localhost:8080/api/auth/signup",
+      {
+        method: "POST",
+        body: data,
+      },
+    );
+
+    if (response.status == 200) {
+      const data = await response.text();
+      console.log(data);
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export async function login(formData: FormData) {
   formData.forEach((data) => {
     if (!data) return;
   });
+
   const data: FormData = new FormData();
   data.append("name", formData.get("user") as string);
   data.append("password", formData.get("password") as string);
-  const response: Response = await fetch("LOGIN_API", {
-    method: "POST",
-    body: data,
-  });
+
+  try {
+    const response: Response = await fetch(
+      "http://localhost:8080/api/auth/login",
+      {
+        method: "POST",
+        body: data,
+      },
+    );
+
+    if (response.status == 200) {
+      const data = await response.text();
+      console.log(data);
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
