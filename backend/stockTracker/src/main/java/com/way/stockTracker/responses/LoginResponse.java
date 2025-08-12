efbcf14022582ba41421c709dbfb24b810d170a9
Way05 <wtan235@gmail.com>
2025-08-11 21:49:42 -0400

    response object for frontend

---
+++ backend/stockTracker/src/main/java/com/way/stockTracker/responses/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.way.stockTracker.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginResponse {
+
+    private String token;
+
+    private long expiresIn;
+
+}
