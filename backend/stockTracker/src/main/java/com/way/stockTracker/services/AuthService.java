477f0ba2afdad65535fe4a3d7c3eb9c64d4c3737
Way05 <wtan235@gmail.com>
2025-08-11 21:50:17 -0400

    extracted AuthController login/signup to DI, handles errors

---
+++ backend/stockTracker/src/main/java/com/way/stockTracker/services/AuthService.java
@@ -0,0 +1,32 @@
+package com.way.stockTracker.services;
+
+import com.way.stockTracker.dto.UserDTO;
+import com.way.stockTracker.models.User;
+
+public class AuthService {
+    private UserService userService;
+    private PasswordService passwordService;
+
+    public User signup(UserDTO input) {
+        User userExists = userService.findUserByUsername(input.getUsername());
+        if (userExists != null) {
+            throw new RuntimeException("User already exists");
+        }
+        User newUser = new User(input.getUsername(), passwordService.hashPassword(input.getPassword()));
+        userService.createUser(newUser);
+        return newUser;
+    }
+
+    public User authenticate(UserDTO input) {
+        User userExists = userService.findUserByUsername(input.getUsername());
+        if (userExists == null) {
+            throw new RuntimeException("User does not exist");
+        }
+        String dbPasswordHash = userExists.getPassword();
+        boolean compare = passwordService.verifyPassword(input.getPassword(), dbPasswordHash);
+        if (!compare) {
+            throw new RuntimeException("Incorrect password");
+        }
+        return userExists;
+    }
+}
