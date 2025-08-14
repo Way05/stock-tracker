b48945850ca52d8cab3c1f6ee056dae46d704542
Wei-en Tan <wtan235@gmail.com>
2025-08-13 21:41:47 -0400

    logout button

--- frontend/stockTracker/src/app/dashboard/page.tsx
+++ frontend/stockTracker/src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
-export default function Dashboard() {
+import { clearJWT } from "../auth";
+import { Button } from "@heroui/button";
+
+export default async function Dashboard() {
   return (
-    <div className="mx-auto flex h-screen items-center justify-center">
+    <div className="mx-auto flex h-screen flex-col items-center justify-center">
       dashboard
+      <Button type="submit" color="success" variant="ghost" onPress={clearJWT}>
+        Log Out
+      </Button>
     </div>
   );
 }
