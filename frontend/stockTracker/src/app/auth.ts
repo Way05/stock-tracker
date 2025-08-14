3c5d001485faa0eb6717991f3066fe2bb4f291f3
Wei-en Tan <wtan235@gmail.com>
2025-08-13 21:41:03 -0400

    function to clear JWT cookie

--- frontend/stockTracker/src/app/auth.ts
+++ frontend/stockTracker/src/app/auth.ts
@@ -1,5 +1,6 @@
 "use server";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function setJWT(jwt: string) {
   const cookieStore = await cookies();
@@ -8,4 +9,10 @@ export async function setJWT(jwt: string) {
     maxAge: 60 * 60 * 24,
     path: "/",
   });
+}
+
+export async function clearJWT() {
+  const cookieStore = await cookies();
+  cookieStore.delete("jwtToken");
+  redirect("/");
 }
