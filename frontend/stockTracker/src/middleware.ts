7b8718e026291067b1baacdc68f29caff669d95c
Wei-en Tan <wtan235@gmail.com>
2025-08-13 21:40:39 -0400

    moved to right location, restricts /dashbaord

---
+++ frontend/stockTracker/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const protectedRoutes: string[] = ["/dashboard"];
+  if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+    const JWT = request.cookies.get("jwtToken");
+
+    if (!JWT) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/dashboard/:path*",
+};
