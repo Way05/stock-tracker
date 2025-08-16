23adaeb5c04a827649744891f0fdf2812336ed62
Wei-en Tan <wtan235@gmail.com>
2025-08-16 08:20:35 -0400

    added post api objects

--- frontend/stockTracker/src/app/dataInterfaces.ts
+++ frontend/stockTracker/src/app/dataInterfaces.ts
@@ -2,3 +2,12 @@ export interface authResponse {
   token: string;
   timeOut: number;
 }
+
+export interface postObject {
+  title: string;
+  content: string;
+}
+
+export interface postResponse {
+  posts: postObject[];
+}
