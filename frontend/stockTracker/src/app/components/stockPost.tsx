681482389ee90a77cba719c4eafa23ff9b30a50b
Wei-en Tan <wtan235@gmail.com>
2025-08-19 14:05:59 -0400

    client side post voting system added

--- frontend/stockTracker/src/app/components/stockPost.tsx
+++ frontend/stockTracker/src/app/components/stockPost.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Button } from "@heroui/button";
 import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
+import { useState } from "react";
 
 type stockProps = {
   title: string;
@@ -10,6 +12,10 @@ type stockProps = {
 };
 
 export default function StockPost(props: stockProps) {
+  const [voted, setVoted] = useState<boolean>(false);
+  const [votes, setVotes] = useState<number>(props.votes);
+  const [isUpVoted, setIsUpVoted] = useState<boolean | null>(null);
+
   return (
     <div className="m-2 w-100">
       <Card className="border-1 border-white/50 p-2 text-center">
@@ -19,11 +25,56 @@ export default function StockPost(props: stockProps) {
           <div className="text-xs text-white/50">
             Posted by {props.author} on {props.date}
           </div>
-          {/* <div>
-            <Button isIconOnly>^</Button>
-            {props.votes}
-            <Button isIconOnly>v</Button>
-          </div> */}
+          <div className="flex gap-2 align-middle">
+            <Button
+              size="sm"
+              className="p-0"
+              isIconOnly
+              onPress={() => {
+                if (voted && isUpVoted) {
+                  setVotes(votes - 1);
+                  setVoted(false);
+                  setIsUpVoted(null);
+                } else if (voted && !isUpVoted) {
+                  setVotes(votes + 2);
+                  setIsUpVoted(true);
+                } else {
+                  setVotes(votes + 1);
+                  setVoted(true);
+                  setIsUpVoted(true);
+                }
+              }}
+              variant={
+                isUpVoted == null ? "ghost" : isUpVoted ? "solid" : "ghost"
+              }
+            >
+              +
+            </Button>
+            {votes ? votes : null}
+            <Button
+              size="sm"
+              isIconOnly
+              onPress={() => {
+                if (voted && isUpVoted) {
+                  setVotes(votes - 2);
+                  setIsUpVoted(false);
+                } else if (voted && !isUpVoted) {
+                  setVotes(votes + 1);
+                  setVoted(false);
+                  setIsUpVoted(null);
+                } else {
+                  setVotes(votes - 1);
+                  setVoted(true);
+                  setIsUpVoted(false);
+                }
+              }}
+              variant={
+                isUpVoted == null ? "ghost" : isUpVoted ? "ghost" : "solid"
+              }
+            >
+              -
+            </Button>
+          </div>
         </CardFooter>
       </Card>
     </div>
