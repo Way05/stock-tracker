cd2636900b05b314a48fa91d5b2261ce7387a270
Way05 <wtan235@gmail.com>
2025-08-22 20:04:27 -0400

    some api setup

---
+++ backend/stockTracker/src/main/java/com/way/stockTracker/controllers/StockController.java
@@ -0,0 +1,22 @@
+package com.way.stockTracker.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stock")
+public class StockController {
+    public void add() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    @GetMapping("/search")
+    public void search() {
+
+    }
+}
