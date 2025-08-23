b2cd2e731996e7089be65bc6b5cf7afcebbd94c5
Way05 <wtan235@gmail.com>
2025-08-22 22:16:22 -0400

    ticker is indexed field

--- backend/stockTracker/src/main/java/com/way/stockTracker/models/Stock.java
+++ backend/stockTracker/src/main/java/com/way/stockTracker/models/Stock.java
@@ -3,6 +3,7 @@ package com.way.stockTracker.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Stock {
     @Id
     private String id;
+    @Indexed
     private String ticker;
     private String company;
     private String price;
