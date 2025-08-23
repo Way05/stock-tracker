ef236ccf801090097369866a521348ea801971f2
Way05 <wtan235@gmail.com>
2025-08-22 20:04:19 -0400

    stock object created

---
+++ backend/stockTracker/src/main/java/com/way/stockTracker/models/Stock.java
@@ -0,0 +1,23 @@
+package com.way.stockTracker.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "Stocks")
+public class Stock {
+    @Id
+    private String id;
+    private String ticker;
+    private String company;
+    private String price;
+
+    public Stock(String ticker, String company, String price) {
+        this.ticker = ticker;
+        this.company = company;
+        this.price = price;
+    }
+}
