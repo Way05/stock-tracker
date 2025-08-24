4e957f6e885fc2cfcc8a5cb279fb1efb715a5951
Wei-en Tan <wtan235@gmail.com>
2025-08-24 18:46:30 -0400

    default add stock button created

--- frontend/stockTracker/src/app/components/stockList.tsx
+++ frontend/stockTracker/src/app/components/stockList.tsx
@@ -1,16 +1,16 @@
-import { Card, CardBody } from "@heroui/card";
+import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
 import StockCard from "./stockCard";
+import { Button } from "@heroui/button";
 
 export default function StockList() {
   return (
     <Card>
       <CardBody className="grid grid-cols-5 gap-1">
-        <StockCard ticker="NVDA" name="Nvidia" price="$0.00 USD"></StockCard>
-        <StockCard ticker="NVDA" name="Nvidia" price="$0.00 USD"></StockCard>
-        <StockCard ticker="NVDA" name="Nvidia" price="$0.00 USD"></StockCard>
-        <StockCard ticker="NVDA" name="Nvidia" price="$0.00 USD"></StockCard>
-        <StockCard ticker="NVDA" name="Nvidia" price="$0.00 USD"></StockCard>
-        <StockCard ticker="NVDA" name="Nvidia" price="$0.00 USD"></StockCard>
+        <div className="aspect-square w-30">
+          <Card className="aspect-square border-1 border-white/50 p-2 text-center">
+            <Button className="aspect-square h-full w-full text-3xl">+</Button>
+          </Card>
+        </div>
       </CardBody>
     </Card>
   );
