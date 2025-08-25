e0224797ab30213fd3380ba4e91df31e4db3cd22
Wei-en Tan <wtan235@gmail.com>
2025-08-25 15:35:48 -0400

    opens drawer on stock select

--- frontend/stockTracker/src/app/components/stockSearch.tsx
+++ frontend/stockTracker/src/app/components/stockSearch.tsx
@@ -3,17 +3,26 @@ import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
 import { Key, useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 import { stockObject } from "../dataInterfaces";
+import ViewStockDrawer from "./viewStockDrawer";
+import { useDisclosure } from "@heroui/use-disclosure";
+import { Button } from "@heroui/button";
 
 const placeholder: stockObject = {
   ticker: "Search for an IPO",
   company: "",
 };
 
+const testStocks: stockObject[] = [{ ticker: "AAPL", company: "aaple" }];
+
 export default function StockSearch() {
-  const [items, setItems] = useState<stockObject[]>([placeholder]);
-  const [selection, setSelection] = useState<Key | null>();
   const [input, setInput] = useState<string>("");
   const [debouncedInput] = useDebounce<string>(input, 500);
+  const [items, setItems] = useState<stockObject[]>([
+    placeholder,
+    ...testStocks,
+  ]);
+  const [selection, setSelection] = useState<Key | null>("Null");
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   function onInputChange(input: string) {
     setInput(input);
@@ -21,7 +30,7 @@ export default function StockSearch() {
 
   function onSelectionChange(key: Key | null) {
     setSelection(key);
-    console.log(key);
+    if (key != null) onOpen();
   }
 
   async function fetchStocks(query: string) {
@@ -66,23 +75,31 @@ export default function StockSearch() {
         onInputChange={onInputChange}
         onSelectionChange={onSelectionChange}
       >
-        {items.map((stock) => {
-          return (
-            <AutocompleteItem key={stock.ticker} textValue={stock.ticker}>
-              <div className="flex grid-rows-2 items-center gap-2">
-                <div className="flex">
-                  <div className="flex flex-col">
-                    <span className="text-small">{stock.ticker}</span>
-                    <span className="text-tiny text-default-400">
-                      {stock.company}
-                    </span>
+        {items &&
+          items.map((stock) => {
+            return (
+              <AutocompleteItem key={stock.ticker} textValue={stock.ticker}>
+                <div className="flex grid-rows-2 items-center gap-2">
+                  <div className="flex">
+                    <div className="flex flex-col">
+                      <span className="text-small">{stock.ticker}</span>
+                      <span className="text-tiny text-default-400">
+                        {stock.company}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </AutocompleteItem>
-          );
-        })}
+              </AutocompleteItem>
+            );
+          })}
       </Autocomplete>
+      <ViewStockDrawer
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+        ticker={selection!.toString()}
+        company="Nvidia"
+      />
     </div>
   );
 }
